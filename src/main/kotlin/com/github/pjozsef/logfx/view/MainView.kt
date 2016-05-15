03915a07204516c9496cb6457862c34cfd04330d
pjozsef <pollak.jozsef1@gmail.com>
2016-05-15 17:27:51 +0200

    Basic layout added for right panel

--- src/main/kotlin/com/github/pjozsef/logfx/view/MainView.kt
+++ src/main/kotlin/com/github/pjozsef/logfx/view/MainView.kt
@@ -4,15 +4,18 @@
 
 package com.github.pjozsef.logfx.view
 
-import javafx.scene.control.Button
+import com.jfoenix.controls.JFXButton
+import com.jfoenix.controls.JFXTextArea
 import javafx.scene.layout.BorderPane
 import tornadofx.View
 
 class MainView : View() {
     override val root: BorderPane by fxml()
-     //val testButton: Button by fxid();
+    val open: JFXButton by fxid()
+    val rules: JFXTextArea by fxid()
 
     init {
-
+        println(open)
+        println(rules)
     }
 }
